93533d3db3e9f8afe530bf1e2f07a509a6dcb53d
anointedthedeveloper <anointedthedeveloper@gmail.com>
2026-04-30 00:41:49 +0100

    Initial Commit

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(
-    import.meta.env.VITE_SUPABASE_URL,
-    import.meta.env.VITE_SUPABASE_ANON_KEY
-);
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || 'https://bclwwzgmezigfhyphdcr.supabase.co';
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjbHd3emdtZXppZ2ZoeXBoZGNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0ODk2NjcsImV4cCI6MjA5MzA2NTY2N30.1jgXFU72suy1U1dPrXzzQcJLyzkcv2BmVLhvJ5xaRHA';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
